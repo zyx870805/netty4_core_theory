04973c137b3276745edb00feeb9b4c03f013a44d
root <zhangyingxuan@maoyan.com>
2021-02-12 15:48:20 +0800

    2021

---
+++ src/main/java/org/example/chapter007/ChatClient.java
@@ -0,0 +1,39 @@
+package org.example.chapter007;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class ChatClient {
+    public ChatClient connect(int port, String host, final String nickName) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+        return this;
+    }
+
+    public static void main(String[] args) {
+        new ChatClient().connect(8080, "localhost", "tom");
+    }
+}
